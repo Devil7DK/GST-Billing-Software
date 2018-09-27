a8f0da31254480bf5e02288560c2a0c8b6cc69f5
Devil7DK <dineshthangavel47@gmail.com>
2018-09-27 21:32:59 +0530

    Add Objects: State, Address & Party

---
+++ GST - Billing Software/Classes/Objects/Address.vb
@@ -0,0 +1,63 @@
+﻿'=========================================================================='
+'                                                                          '
+'                    (C) Copyright 2018 Devil7 Softwares.                  '
+'                                                                          '
+' Licensed under the Apache License, Version 2.0 (the "License");          '
+' you may not use this file except in compliance with the License.         '
+' You may obtain a copy of the License at                                  '
+'                                                                          '
+'                http://www.apache.org/licenses/LICENSE-2.0                '
+'                                                                          '
+' Unless required by applicable law or agreed to in writing, software      '
+' distributed under the License is distributed on an "AS IS" BASIS,        '
+' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. '
+' See the License for the specific language governing permissions and      '
+' limitations under the License.                                           '
+'                                                                          '
+' Contributors :                                                           '
+'     Dineshkumar T                                                        '
+'                                                                          '
+'=========================================================================='
+
+Imports System.ComponentModel
+
+Namespace Classes.Objects
+    Public Class Address
+
+#Region "Fields/Properties"
+        <DisplayName("Address Line 1")>
+        Property AddressLine1 As String
+        <DisplayName("Address Line 2")>
+        Property AddressLine2 As String
+        Property City As String
+        <DisplayName("PIN Code")>
+        Property PinCode As Integer
+        Property State As State
+#End Region
+
+#Region "Constructors"
+        Sub New()
+            Me.AddressLine1 = ""
+            Me.AddressLine2 = ""
+            Me.City = ""
+            Me.PinCode = 0
+            Me.State = New State
+        End Sub
+
+        Sub New(ByVal AddressLine1 As String, ByVal AddressLine2 As String, ByVal City As String, ByVal PinCode As Integer, ByVal State As State)
+            Me.AddressLine1 = AddressLine1
+            Me.AddressLine2 = AddressLine2
+            Me.City = City
+            Me.PinCode = PinCode
+            Me.State = State
+        End Sub
+#End Region
+
+#Region "Subs/Functions"
+        Public Overrides Function ToString() As String
+            Return String.Join(vbNewLine, AddressLine1, AddressLine2, String.Format("{0} - {1}", City, PinCode.ToString("000000")))
+        End Function
+#End Region
+
+    End Class
+End Namespace
---
+++ GST - Billing Software/Classes/Objects/Party.vb
@@ -0,0 +1,67 @@
+﻿'=========================================================================='
+'                                                                          '
+'                    (C) Copyright 2018 Devil7 Softwares.                  '
+'                                                                          '
+' Licensed under the Apache License, Version 2.0 (the "License");          '
+' you may not use this file except in compliance with the License.         '
+' You may obtain a copy of the License at                                  '
+'                                                                          '
+'                http://www.apache.org/licenses/LICENSE-2.0                '
+'                                                                          '
+' Unless required by applicable law or agreed to in writing, software      '
+' distributed under the License is distributed on an "AS IS" BASIS,        '
+' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. '
+' See the License for the specific language governing permissions and      '
+' limitations under the License.                                           '
+'                                                                          '
+' Contributors :                                                           '
+'     Dineshkumar T                                                        '
+'                                                                          '
+'=========================================================================='
+
+Imports System.ComponentModel
+
+Namespace Classes.Objects
+    Public Class Party
+
+#Region "Fields/Properties"
+        <Browsable(False)>
+        Property ID As Integer
+        Property Name As String
+        Property GSTIN As String
+        Property Address As Address
+        Property Mobile As String
+        ReadOnly Property State As State
+            Get
+                Return If(Address Is Nothing, New State, Address.State)
+            End Get
+        End Property
+        <DisplayName("State Code")>
+        ReadOnly Property StateCode As Integer
+            Get
+                Return If(Address Is Nothing, 0, Address.State.Code)
+            End Get
+        End Property
+#End Region
+
+#Region "Constructors"
+        Sub New()
+            Me.ID = -1
+            Me.Name = ""
+            Me.GSTIN = ""
+            Me.Address = New Address
+            Me.Mobile = ""
+        End Sub
+
+        Sub New(ByVal ID As Integer, ByVal Name As String, ByVal GSTIN As String, ByVal Address As Address, ByVal Mobile As String)
+            Me.ID = ID
+            Me.Name = Name
+            Me.GSTIN = GSTIN
+            Me.Address = New Address
+            Me.Mobile = Mobile
+        End Sub
+#End Region
+
+
+    End Class
+End Namespace
