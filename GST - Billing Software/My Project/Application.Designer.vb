51a0405d768d8b42837151e13e73757f11d85554
Devil7DK <dineshthangavel47@gmail.com>
2018-09-27 20:45:52 +0530

    Use RibbonForm for MainForm

--- GST - Billing Software/My Project/Application.Designer.vb
+++ GST - Billing Software/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Devil7.Billing.GST.Form1
+            Me.MainForm = Global.Devil7.Billing.GST.frm_Main
         End Sub
     End Class
 End Namespace
